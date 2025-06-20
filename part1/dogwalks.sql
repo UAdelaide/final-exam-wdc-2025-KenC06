dc5e56b571a2d76d663a5deddc91efb9f2541e68
KenC06 <a1939742@adelaide.edu.au>
2025-06-20 02:17:45 +0000

    [automatic commit] 2025-06-20 02:17:45 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -6,7 +6,7 @@ CREATE TABLE Users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    role ENUM('owner', 'walker') NOT NULL,
+    role ENUM('owner', 'walker') NOT NULL,  
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
