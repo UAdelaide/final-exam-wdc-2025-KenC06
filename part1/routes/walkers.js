7a62699b8171800c0ed9b5ec2ddba87edf33fe23
KenC06 <a1939742@adelaide.edu.au>
2025-06-20 05:31:27 +0000

    [automatic commit] 2025-06-20 05:31:27 UTC

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -2,10 +2,29 @@ const express = require('express');
 const pool = require('../db');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-const sql =
-
-
-
-
+router.get('/', async (req, res, next) => {
+  try {
+    const [rows] = await pool.query(`
+      SELECT u.username          AS walker_username,
+             COUNT(r.rating_id)  AS total_ratings,
+             AVG(r.rating)       AS average_rating,
+             COUNT(
+               CASE
+                 WHEN wa.status='accepted'
+                  AND wr.status='completed'
+               THEN 1 END
+             )                    AS completed_walks
+      FROM Users u
+      LEFT JOIN WalkRatings      r  ON u.user_id = r.walker_id
+      LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
+      LEFT JOIN WalkRequests     wr ON wa.request_id = wr.request_id
+      WHERE u.role = 'walker'
+      GROUP BY u.username
+    `);
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
 });
+
+module.exports = router;
