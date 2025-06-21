29072147842512864f678e3096d747dc28cdb787
KenC06 <a1939742@adelaide.edu.au>
2025-06-21 01:26:50 +0000

    [automatic commit] 2025-06-21 01:26:50 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -9,5 +9,10 @@ router.get('/', async (req,res) => {
 }
 try {
 
-const[ rows]
+const[ rows ] = await db.query(
+    `SELECT dog_id, name
+         FROM Dogs
+        WHERE owner_id = ?`,
+      [req.session.user.user_id]
+      
 }
