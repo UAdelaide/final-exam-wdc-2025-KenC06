1506c82536f77ebb69de37947f094c80493696bb
KenC06 <a1939742@adelaide.edu.au>
2025-06-21 01:27:50 +0000

    [automatic commit] 2025-06-21 01:27:50 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/', async (req,res) => {
-    if(!req.session.user){
-        return res.status(401).json({error: 'Not authenticated'});
+router.get('/', async (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ error: 'Not authenticated' });
     }
 }
 try {
 
-const[ rows ] = await db.query(
-    `SELECT dog_id, name
+    const [rows] = await db.query(
+        `SELECT dog_id, name
          FROM Dogs
         WHERE owner_id = ?`,
-      [req.session.user.user_id]
-);
-res.json(rows);
-}catch(err){
+        [req.session.user.user_id]
+    );
+    res.json(rows);
+} catch (err) {
     console.error(err);
-    res.status(500).json({error: 'unable to fetch dogs'})
+    res.status(500).json({ error: 'unable to fetch dogs' });
 }
+});
