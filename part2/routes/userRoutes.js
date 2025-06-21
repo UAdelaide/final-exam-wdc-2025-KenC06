38fe0305679c3a58839ee858e4d078d5064bfd6c
KenC06 <a1939742@adelaide.edu.au>
2025-06-21 01:12:46 +0000

    [automatic commit] 2025-06-21 01:12:46 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,6 +58,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// logout logic
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) return res.status(500).json({ error: 'Logout failed' });
